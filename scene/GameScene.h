405865c4c48e0e63608beea7c45182a69526c622
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-04-17 12:21:03 +0900

    01_02_確認課題

--- scene/GameScene.h
+++ scene/GameScene.h
@@ -8,6 +8,8 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
+#include "DebugCamera.h"
+
 
 /// <summary>
 /// ゲームシーン
@@ -19,7 +21,7 @@ public: // メンバ関数
 	/// コンストクラタ
 	/// </summary>
 	GameScene();
-
+	
 	/// <summary>
 	/// デストラクタ
 	/// </summary>
@@ -44,8 +46,21 @@ private: // メンバ変数
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+	uint32_t textureHandle_ = 0;
+
+	uint32_t soundDataHandle_ = 0;
+	uint32_t voiceHandle_ = 0;
+
+	Sprite* sprite_ = nullptr;
+	Model* model_ = nullptr;
+	
+	WorldTransform worldTransform_;
+	ViewProjection viewProjection_;//カメラ
+	float inputFloat3[3] = {0, 0, 0};
+
+	DebugCamera* debugCamera_ = nullptr;
 
 	/// <summary>
-	/// ゲームシーン用
+	/// ゲーム シーン用
 	/// </summary>
 };
