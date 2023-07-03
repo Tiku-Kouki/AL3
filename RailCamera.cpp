8084ab219272e2243c705534a6ba7879170ffb1b
Tiku-Kouki <k022g0079@g.neec.ac.jp>
2023-07-03 12:43:52 +0900

    02_12_確認課題

--- RailCamera.cpp
+++ RailCamera.cpp
@@ -15,8 +15,8 @@ void RailCamera::Initalize() {
 
 void RailCamera::Update() {
 
-	worldTransform_.translation_.z -= 0.03f;
-	worldTransform_.rotation_.y -= 0.01f;
+	worldTransform_.translation_.z -= 0.01f;
+	worldTransform_.rotation_.y += 0.01f;
 
 	worldTransform_.matWorld_ = MakeAffineMatrix(
 	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
--- Skydome.cpp
+++ Skydome.cpp
@@ -8,7 +8,7 @@ void Skydome::Initialize(Model* model) {
 	
 
 
-	worldTransform_.scale_ = {50.0f, 50.0f, 50.0f};
+	worldTransform_.scale_ = {150.0f, 150.0f, 150.0f};
 
 	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
 
